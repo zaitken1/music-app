eb722ea7a5ee142cd272fbff3e2ed6edeb71b4cd
zaitken <zaraaitken18@gmail.com>
2023-01-09 10:10:58 +0000

    dynamically create YouTube button and link to video based on user search input

--- scripts/script.js
+++ scripts/script.js
@@ -12,55 +12,57 @@ var sophAPIKey = 'AIzaSyD5r9mHgGwHO-m77puQByqYHX7gYO-LIsg';
 var baseYouTubeURL = `https://www.googleapis.com/youtube/v3/search?key=${youtubeAPIKey}&maxResults=1&order=relevance&`;
 
 //YouTube API call to create button to link to YouTube based on user search input
-function getVideo (event) {
+function getVideoLink(event) {
   event.preventDefault();
   var track = searchInput.val().trim().toLowerCase();
 
   if (track) {
-      console.log('search submitted');
 
-      function inputSubmit(trackName){
-          $.get(baseYouTubeURL + `q=${trackName}`)
-              .then(function (currentData){
-                  console.log(currentData);
-              })
+    function inputSubmit(trackName) {
+      $.get(baseYouTubeURL + `q=${trackName}`)
+        .then(function (currentData) {
+          console.log(currentData);
 
-              searchInput.val('');
-              songCard.html('');
+          var videoID = currentData.items[0].id.videoId;
 
-              songCard.html(`
-              <button type="button" class="btn btn-primary btn-lg yt-btn"><i class="fab fa-youtube"></i>Watch song on YouTube</button>
-              `)
-      }
+          searchInput.val('');
+          songCard.html('');
 
-  } else {
-      console.log("empty");
+          songCard.html(`
+              <a href="https://www.youtube.com/watch?v=${videoID}" target="_blank"><button type="button" class="btn btn-primary btn-lg yt-btn"><i class="fab fa-youtube"></i>Watch song on YouTube</button></a>
+              `);
+          
+          var youtubeBtn = $('.yt-btn');
+
+
+        })
+    } 
+    }
+
+    inputSubmit(track);
   }
 
-  inputSubmit(track);
-}
-
-// var musicDataSearch = "rizzle";
+  // var musicDataSearch = "rizzle";
 
 
 
-// var musicDataSearch = searchInput.val();
-function getArtistNames(event) {
-  event.preventDefault();
-  var musicDataSearch = searchInput.val();
+  // var musicDataSearch = searchInput.val();
+  function getArtistNames(event) {
+    event.preventDefault();
+    var musicDataSearch = searchInput.val();
 
-  if (musicDataSearch) {
-    $.get(
-      `
+    if (musicDataSearch) {
+      $.get(
+        `
   https://api.allorigins.win/get?url=${encodeURIComponent(
-    `https://api.musixmatch.com/ws/1.1/artist.search?q_artist=${musicDataSearch}&page_size=5&apikey=ad3a142fa0bfd7ef82851240e57a5429`
-  )}
+          `https://api.musixmatch.com/ws/1.1/artist.search?q_artist=${musicDataSearch}&page_size=5&apikey=ad3a142fa0bfd7ef82851240e57a5429`
+        )}
 `
-    ).then(function (data) {
-      data = JSON.parse(data.contents);
-      lyricsSection.html("");
+      ).then(function (data) {
+        data = JSON.parse(data.contents);
+        lyricsSection.html("");
 
-      lyricsSection.html(`
+        lyricsSection.html(`
     <div>
       <h4>Similar to your search...</h4>
       <ul>
@@ -72,13 +74,13 @@ function getArtistNames(event) {
       </ul>
     </div>
     `);
-    });
+      });
+    }
   }
-}
 
-//Starting function on search button click
-function init() {
-  searchBtn.click(getArtistNames, getVideo);
-}
+  //Starting function on search button click
+  function init() {
+    searchBtn.click(getArtistNames, getVideoLink);
+  }
 
-init();
+  init();
